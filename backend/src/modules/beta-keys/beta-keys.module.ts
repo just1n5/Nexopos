d29f18e1137b685c1b8d881838da60f966c61349
serna001 <jserna@cloutionsas.com>
2025-10-20 21:06:21 -0500

    fix(backend): make EmailModule global to resolve DI issue

--- backend/src/modules/beta-keys/beta-keys.module.ts
+++ backend/src/modules/beta-keys/beta-keys.module.ts
@@ -3,10 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BetaKeysService } from './beta-keys.service';
 import { BetaKeysController } from './beta-keys.controller';
 import { BetaKey } from './entities/beta-key.entity';
-import { EmailModule } from '../email/email.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BetaKey]), EmailModule],
+  imports: [TypeOrmModule.forFeature([BetaKey])],
   controllers: [BetaKeysController],
   providers: [BetaKeysService],
   exports: [BetaKeysService],
